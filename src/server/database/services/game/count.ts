4eea4795d2573cb0b7114c613e7a95e9055e7864
alex lima <delimaferreiraalex@gmail.com>
2024-04-17 20:21:49 -0300

    add count considering if a entry is deleted

---
+++ src/server/database/services/game/count.ts
@@ -0,0 +1,19 @@
+import { QueryFailedError } from "typeorm";
+import connection from "../../connection.js";
+import { Game } from "../../entities/Game.js";
+
+const count = async (): Promise<number> => {
+    try {
+        const repository = connection.getRepository(Game);
+
+        return await repository.count({ where: { isDeleted: false } });
+    } catch (err: unknown) {
+        if (err instanceof QueryFailedError) {
+            console.error(err.message);
+            return 0;
+        }
+        return 0;
+    }
+};
+
+export { count };
