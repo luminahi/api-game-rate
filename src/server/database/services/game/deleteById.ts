5daa817018676aaf86481a9f180025eeba1cae97
alex lima <delimaferreiraalex@gmail.com>
2024-04-21 00:16:36 -0300

    fix: add await to 'deletion'

--- src/server/database/services/game/deleteById.ts
+++ src/server/database/services/game/deleteById.ts
@@ -5,7 +5,7 @@ import { Game } from "../../entities/Game.js";
 const deleteById = async (id: number): Promise<void> => {
     try {
         const repository = connection.getRepository(Game);
-        repository.update({ id }, { isDeleted: true });
+        await repository.update({ id }, { isDeleted: true });
     } catch (err: unknown) {
         if (err instanceof QueryFailedError) {
             console.error(err.message);
