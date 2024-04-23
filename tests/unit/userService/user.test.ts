8f988ce9020fdbf0bec7bf47bfb35d5697c36879
alex lima <delimaferreiraalex@gmail.com>
2024-04-22 21:49:14 -0300

    add basic unit tests for user entity

--- tests/unit/userService/user.test.ts
+++ tests/unit/userService/user.test.ts
@@ -1,5 +1,58 @@
-describe("user service test suite", () => {
-    test.only("only this one", () => {
-        expect(2).toBeGreaterThan(0);
+import connection from "../../../src/server/database/connection.js";
+import { User } from "../../../src/server/database/entities/User.js";
+import { userService } from "../../../src/server/database/services/user/index.js";
+
+beforeAll(async () => {
+    const repository = connection.getRepository(User);
+
+    const user1 = new User();
+    const user2 = new User();
+    const user3 = new User();
+
+    user1.username = "lima alex";
+    user1.email = "alex@mail.com";
+    user1.password = "00001111";
+
+    user2.username = "sarah sousa";
+    user2.email = "sarah@mail.com";
+    user2.password = "11001111";
+
+    user3.username = "karin doe";
+    user3.email = "karin@mail.com";
+    user3.password = "00110011";
+
+    await repository.save([user1, user2, user3]);
+});
+
+describe("userService", () => {
+    it("count", async () => {
+        const userCount = await userService.count();
+
+        expect(userCount).not.toBeUndefined();
+        expect(userCount).toBe(3);
+    });
+
+    it("get one user by id", async () => {
+        const user = await userService.getById(1);
+
+        expect(user).not.toBeNull();
+        expect(user).toBeInstanceOf(User);
+        expect(user?.id).toBe(1);
+    });
+
+    it("creates a entry", async () => {
+        const user = new User();
+
+        user.username = "fourth";
+        user.email = "fourth@mail.com";
+        user.password = "44441111";
+
+        const savedUser = await userService.create(user);
+
+        expect(savedUser).not.toBeNull();
+        expect(savedUser?.username).toBe("fourth");
+        expect(savedUser?.email).toBe("fourth@mail.com");
+        expect(savedUser?.password).toBe("44441111");
+        expect(savedUser?.id).toBeDefined();
     });
 });
