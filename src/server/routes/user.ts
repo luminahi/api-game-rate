9b26ab2338a30d5943e4d887eab68b8d5bf2ddbf
alex lima <delimaferreiraalex@gmail.com>
2024-04-25 19:28:01 -0300

    feat: add new user route

--- src/server/routes/user.ts
+++ src/server/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { signIn, signUp } from "../controllers/user/index.js";
+import { signUpValidation } from "../database/validations/user/signUpValidation.js";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.json({ user: "router" });
-});
+router.post("/signin", signIn);
+router.post("/signup", signUpValidation, signUp);
 
 export { router as userRouter };
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { gameRouter } from "./routes/game.js";
+import { userRouter } from "./routes/user.js";
 
 const server = express();
 server.use(express.json({}));
 
 server.use("/api/v1/games", gameRouter);
+server.use("/api/v1/ratings", (_, res) => res.json({ msg: "not implemented" }));
+server.use("/auth", userRouter);
 
 server.get("/", async (req, res) => {
     res.json({ msg: "message" });
