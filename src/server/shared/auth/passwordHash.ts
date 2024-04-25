794f58ae7591463791328813f753683e1360c5dd
alex lima <delimaferreiraalex@gmail.com>
2024-04-25 19:28:50 -0300

    ref: fix hashing method return type

--- src/server/shared/auth/passwordHash.ts
+++ src/server/shared/auth/passwordHash.ts
@@ -1,15 +1,17 @@
 import bcrypt from "bcryptjs";
 
-const hashPassword = async (password: string) => {
+const hashPassword = async (password: string): Promise<string> => {
     try {
         let hashedPassword = "";
         const salt = await bcrypt.genSalt();
         hashedPassword = await bcrypt.hash(password, salt);
+
         return hashedPassword;
     } catch (err: unknown) {
         if (err instanceof Error) {
             console.error(err.message);
         }
+        return "";
     }
 };
 
