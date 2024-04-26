26bdb41a8caba77e281b7ba0bdb348e47bf59bbc
alex lima <delimaferreiraalex@gmail.com>
2024-04-26 19:04:02 -0300

    fix: now it properly hash the password before update

--- src/server/database/services/user/updateById.ts
+++ src/server/database/services/user/updateById.ts
@@ -1,10 +1,13 @@
 import { QueryFailedError } from "typeorm";
 import connection from "../../connection.js";
 import { User } from "../../entities/User.js";
+import { hashPassword } from "../../../shared/util/passwordUtil.js";
 
 const updateById = async (id: number, user: User): Promise<void> => {
     try {
         const repository = connection.getRepository(User);
+
+        user.password = await hashPassword(user.password);
 
         await repository.update({ id }, user);
     } catch (err: unknown) {
