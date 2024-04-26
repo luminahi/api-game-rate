e58c2dce36bd6defd10b3493b0a846967cdcc1ae
alex lima <delimaferreiraalex@gmail.com>
2024-04-26 19:12:39 -0300

    ref: moved 'access control' middleware to root of game endpoint

--- src/server/routes/game.ts
+++ src/server/routes/game.ts
@@ -9,12 +9,11 @@ import {
 } from "../controllers/game/index.js";
 import { bodyValidation } from "../shared/middlewares/validations/game/bodyValidation.js";
 import { optionalBodyValidation } from "../shared/middlewares/validations/game/optionalBodyValidation.js";
-import { accessControl } from "../shared/middlewares/auth/accessControl.js";
 
 const router = Router();
 
 router.post("/", bodyValidation, create);
-router.get("/", accessControl, getAll);
+router.get("/", getAll);
 router.get("/:id", getById);
 router.put("/:id", bodyValidation, updateById);
 router.patch("/:id", optionalBodyValidation, patchById);
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { gameRouter } from "./routes/game.js";
 import { userRouter } from "./routes/user.js";
+import { accessControl } from "./shared/middlewares/auth/accessControl.js";
 
 const server = express();
 server.use(express.json({}));
 
-server.use("/api/v1/games", gameRouter);
+server.use("/api/v1/games", accessControl, gameRouter);
 server.use("/api/v1/ratings", (_, res) => res.json({ msg: "not implemented" }));
 server.use("/auth", userRouter);
 
