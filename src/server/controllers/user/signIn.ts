2f248f7a6b151e6f526308b342f28a31d156b27d
alex lima <delimaferreiraalex@gmail.com>
2024-04-25 21:41:47 -0300

    feat: add authentication

--- src/server/controllers/user/signIn.ts
+++ src/server/controllers/user/signIn.ts
@@ -1,8 +1,21 @@
 import { Handler } from "express";
+import { generateAccessToken } from "../../shared/util/jwtUtil.js";
+import { userService } from "../../database/services/user/index.js";
+import { verifyPassword } from "../../shared/util/passwordUtil.js";
 
-const signIn: Handler = (req, res) => {
-    console.log(req.body);
-    res.status(200).json({ dataSent: req.body });
+const signIn: Handler = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await userService.getByEmail(email);
+    if (!user) return res.status(401).json({ msg: "invalid email/password" });
+
+    const isValidPassword = await verifyPassword(password, user.password);
+    if (!isValidPassword)
+        return res.status(401).json({ msg: "invalid email/password" });
+
+    const token = generateAccessToken({ username: user.username, email });
+
+    return res.status(200).json({ token });
 };
 
 export { signIn };
