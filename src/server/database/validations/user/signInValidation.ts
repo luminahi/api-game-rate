e8fd8c4d489943a2cf906282ff2d1c4ef1960010
alex lima <delimaferreiraalex@gmail.com>
2024-04-25 21:46:26 -0300

    add validation for signin

---
+++ src/server/database/validations/user/signInValidation.ts
@@ -0,0 +1,21 @@
+import { Handler } from "express";
+import { Schema, ValidationError, object, string } from "yup";
+import { IUser } from "../../entities/IUser.js";
+
+const userSchema: Schema<Omit<IUser, "username">> = object({
+    email: string().email().nonNullable().required(),
+    password: string().min(8).required(),
+});
+
+const signInValidation: Handler = async (req, res, next) => {
+    try {
+        await userSchema.validate(req.body, { abortEarly: false });
+        return next();
+    } catch (err: unknown) {
+        if (err instanceof ValidationError) {
+            return res.status(400).json({ errors: { ...err.errors } });
+        }
+    }
+};
+
+export { signInValidation };
