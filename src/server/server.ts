c83eb8f379deed1eb487636e403d4e889d9dcdb8
alex lima <delimaferreiraalex@gmail.com>
2024-04-28 17:22:09 -0300

    feat: add rating route

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,16 +2,17 @@ import express from "express";
 import { gameRouter } from "./routes/game.js";
 import { userRouter } from "./routes/user.js";
 import { accessControl } from "./shared/middlewares/auth/accessControl.js";
+import { ratingRouter } from "./routes/rating.js";
 
 const server = express();
 server.use(express.json({}));
 
 server.use("/api/v1/games", accessControl, gameRouter);
-server.use("/api/v1/ratings", (_, res) => res.json({ msg: "not implemented" }));
+server.use("/api/v1/ratings", ratingRouter);
 server.use("/auth", userRouter);
 
 server.get("/", async (req, res) => {
-    res.json({ msg: "message" });
+    res.json({ msg: "index" });
 });
 
 export { server };
