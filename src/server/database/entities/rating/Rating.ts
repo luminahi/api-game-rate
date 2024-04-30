a4cc230232f4aae94ecb42ed63063727946641be
alex lima <delimaferreiraalex@gmail.com>
2024-04-30 15:23:43 -0300

    ref: add range validation to rating

--- src/server/database/entities/rating/Rating.ts
+++ src/server/database/entities/rating/Rating.ts
@@ -1,13 +1,23 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import {
+    Column,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    Unique,
+} from "typeorm";
 import { User } from "../user/User.js";
 import { Game } from "../game/Game.js";
 import { IRating } from "./IRating.js";
+import { Max, Min } from "class-validator";
 
+@Unique("unique_rate", ["user", "game"])
 @Entity({ name: "user_game" })
 export class Rating implements IRating {
     @PrimaryGeneratedColumn()
     id: number;
 
+    @Max(10)
+    @Min(0)
     @Column()
     rating: number;
 
