6a5d390b158d6914caf3a808056ff63749284a6e
alex lima <delimaferreiraalex@gmail.com>
2024-04-29 21:01:15 -0300

    ref: add rating interface

---
+++ src/server/database/entities/rating/IRating.ts
@@ -0,0 +1,6 @@
+interface IRating {
+    id?: number;
+    rating: number;
+}
+
+export { IRating };
--- src/server/database/entities/rating/Rating.ts
+++ src/server/database/entities/rating/Rating.ts
@@ -1,9 +1,10 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "../user/User.js";
 import { Game } from "../game/Game.js";
+import { IRating } from "./IRating.js";
 
 @Entity({ name: "user_game" })
-export class Rating {
+export class Rating implements IRating {
     @PrimaryGeneratedColumn()
     id: number;
 
