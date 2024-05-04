a6457dbdc6394a2e111435ab3f8bcf6373effb0e
alex lima <delimaferreiraalex@gmail.com>
2024-05-04 19:58:51 -0300

    ref: no longer wrap the result in a object

--- src/server/controllers/rating/getAll.ts
+++ src/server/controllers/rating/getAll.ts
@@ -3,7 +3,7 @@ import { ratingService } from "../../database/services/rating/index.js";
 
 const getAll: Handler = async (req, res) => {
     const ratings = await ratingService.getAll();
-    res.status(200).json({ ratings });
+    res.status(200).json(ratings);
 };
 
 export { getAll };
