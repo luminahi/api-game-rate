35cec8acdd65fbd9738472df5636956f6352e44f
alex lima <delimaferreiraalex@gmail.com>
2024-05-07 19:51:45 -0300

    feat: add `.env`  generation file

---
+++ generate-env.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import { randomBytes } from "crypto";
+import fs from "fs";
+import path from "path";
+
+const settings = {
+    port: "3000",
+    host: "localhost",
+    node_env: "development",
+    jwt_secret: randomBytes(32).toString("base64"),
+};
+
+/**
+ *
+ * @param {object} args
+ */
+const generateEnv = (args) => {
+    const filePath = path.resolve(".env");
+    const lines = Object.entries(args);
+    let fd;
+
+    try {
+        fd = fs.openSync(filePath, "w");
+
+        lines.forEach((line) => {
+            fs.writeFileSync(fd, `${line[0]}="${line[1]}"\n`);
+        });
+    } catch (err) {
+        if (err instanceof Error) console.error(err.message);
+    } finally {
+        if (fd) fs.closeSync(fd);
+    }
+};
+
+generateEnv(settings);
