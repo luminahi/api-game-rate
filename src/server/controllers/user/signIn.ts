46bac27e1198c82f7ec0613215bc9cb8e39dfa6f
alex lima <delimaferreiraalex@gmail.com>
2024-05-11 16:23:44 -0300

    ref: updates user actions with `Result`
    modified files:
    signIn.ts
    signUp.ts

--- src/server/controllers/user/signIn.ts
+++ src/server/controllers/user/signIn.ts
@@ -2,18 +2,27 @@ import { Handler } from "express";
 import { generateAccessToken } from "../../shared/util/jwtUtil.js";
 import { userService } from "../../database/services/user/index.js";
 import { verifyPassword } from "../../shared/util/passwordUtil.js";
+import { Result } from "../../shared/util/Result.js";
 
-const signIn: Handler = async (req, res) => {
+const signIn: Handler = async (req, res, next) => {
     const { email, password } = req.body;
 
-    const user = await userService.getByEmail(email);
-    if (!user) return res.status(401).json({ msg: "invalid email/password" });
+    const result = await userService.getByEmail(email);
+    if (result.isFailure())
+        return next(Result.asFailure(401, "invalid email/password"));
 
-    const isValidPassword = await verifyPassword(password, user.password);
+    const isValidPassword = await verifyPassword(
+        password,
+        result.unwrap().password
+    );
+
     if (!isValidPassword)
-        return res.status(401).json({ msg: "invalid email/password" });
+        return next(Result.asFailure(401, "invalid email/password"));
 
-    const token = generateAccessToken({ username: user.username, email });
+    const token = generateAccessToken({
+        username: result.unwrap().username,
+        email,
+    });
 
     return res.status(200).json({ token });
 };
--- src/server/controllers/user/signUp.ts
+++ src/server/controllers/user/signUp.ts
@@ -1,11 +1,13 @@
 import { Handler } from "express";
 import { userService } from "../../database/services/user/index.js";
 
-const signUp: Handler = async (req, res) => {
-    const savedUser = await userService.create(req.body);
-    if (!savedUser)
-        return res.status(400).json({ error: "user could not be registered" });
-    return res.status(201).json({ msg: "user registered" });
+const signUp: Handler = async (req, res, next) => {
+    const result = await userService.create(req.body);
+
+    if (result.isFailure()) return next(result);
+
+    const { id, email, username } = result.unwrap();
+    return res.status(201).json({ user: { id, email, username } });
 };
 
 export { signUp };
