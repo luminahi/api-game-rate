ed1ac2e1850c271c64dddf3abc8f4e04540f629b
alex lima <delimaferreiraalex@gmail.com>
2024-05-12 19:59:26 -0300

    test: game tests now use beforeEach
    using beforeEach instead of beforeAll makes count based tests easier to implement.

--- tests/unit/server/database/services/game/game.test.ts
+++ tests/unit/server/database/services/game/game.test.ts
@@ -1,9 +1,10 @@
 import { gameService } from "../../../../../../src/server/database/services/game/index.js";
 import { Game } from "../../../../../../src/server/database/entities/game/Game.js";
-import connection from "../../../../../../src/server/database/connection.js";
 import { fail } from "assert";
+import connection from "../../../../../../src/server/database/connection.js";
 
-beforeAll(async () => {
+beforeEach(async () => {
+    await connection.synchronize(true);
     const repository = connection.getRepository(Game);
 
     const game1 = new Game();
@@ -36,26 +37,39 @@ describe("gameService", () => {
         expect(game.name).toBeDefined();
     });
 
-    it("creates a game", async () => {
+    it("creates a game and counts", async () => {
         const game = new Game();
         game.name = "The Fourth Game";
-        const result = await gameService.create(game);
-        const newGame = result.unwrap();
+
+        const createResult = await gameService.create(game);
+        const newGame = createResult.unwrap();
 
         if (!newGame) fail();
+
+        const countResult = await gameService.count();
+        const count = countResult.unwrap();
+
+        if (!count) fail();
 
         expect(newGame).toBeInstanceOf(Game);
         expect(newGame.name).toBe(game.name);
         expect(newGame.id).toBeDefined();
+        expect(count).toBe(4);
     });
 
-    it("deletes a game", async () => {
+    it("deletes a game and counts", async () => {
         const deleteResult = await gameService.deleteById(1);
         const affected = deleteResult.unwrap();
 
         if (!affected) fail();
 
+        const countResult = await gameService.count();
+        const count = countResult.unwrap();
+
+        if (!count) fail();
+
         const getResult = await gameService.getById(1);
         expect(getResult.unwrap).toThrow();
+        expect(count).toBe(2);
     });
 });
