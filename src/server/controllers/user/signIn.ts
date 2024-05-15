c9053c25d5bfd536c17f071fce64ed4eccd21f04
alex lima <delimaferreiraalex@gmail.com>
2024-05-15 13:35:22 -0300

    ref: moved user verification code to user service

--- src/server/controllers/user/signIn.ts
+++ src/server/controllers/user/signIn.ts
@@ -1,23 +1,13 @@
 import { Handler } from "express";
 import { generateAccessToken } from "../../shared/util/jwtUtil.js";
 import { userService } from "../../database/services/user/index.js";
-import { verifyPassword } from "../../shared/util/passwordUtil.js";
-import { Result } from "../../shared/util/Result.js";
 
 const signIn: Handler = async (req, res, next) => {
     const { email, password } = req.body;
 
-    const result = await userService.getByEmail(email);
-    if (result.isFailure())
-        return next(Result.asFailure(401, "invalid email/password"));
+    const result = await userService.verifyUser(email, password);
 
-    const isValidPassword = await verifyPassword(
-        password,
-        result.unwrap().password
-    );
-
-    if (!isValidPassword)
-        return next(Result.asFailure(401, "invalid email/password"));
+    if (result.isFailure()) return next(result);
 
     const token = generateAccessToken({
         username: result.unwrap().username,
