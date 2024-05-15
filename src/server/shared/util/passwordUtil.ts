46576224dfa904839a5e62e423d809d8451bd945
alex lima <delimaferreiraalex@gmail.com>
2024-05-15 09:45:47 -0300

    ref: now it throws a error instead
    instead of returning a empty string, it throws the same error reusing
    the try catch of the calling function

--- src/server/shared/util/passwordUtil.ts
+++ src/server/shared/util/passwordUtil.ts
@@ -2,16 +2,17 @@ import bcrypt from "bcryptjs";
 
 const hashPassword = async (password: string): Promise<string> => {
     try {
-        let hashedPassword = "";
         const salt = await bcrypt.genSalt();
-        hashedPassword = await bcrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         return hashedPassword;
     } catch (err: unknown) {
         if (err instanceof Error) {
             console.error(err.message);
+            throw err;
         }
-        return "";
+
+        throw err;
     }
 };
 
