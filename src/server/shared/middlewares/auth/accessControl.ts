1f22eb838040ec56e9fe27dd86a8048db5e0ab9a
alex lima <delimaferreiraalex@gmail.com>
2024-05-15 15:28:29 -0300

    ref: wrap middleware in a try catch
    verifyAccessToken now throws a Error when jwt_secret is undefined

--- src/server/shared/middlewares/auth/accessControl.ts
+++ src/server/shared/middlewares/auth/accessControl.ts
@@ -2,23 +2,32 @@ import { Handler } from "express";
 import { verifyAccessToken } from "../../util/jwtUtil.js";
 
 const accessControl: Handler = (req, res, next) => {
-    if (!req.headers.authorization)
-        return res.status(401).json({ error: "not authorized" });
+    try {
+        if (!req.headers.authorization)
+            return res.status(401).json({ error: "not authorized" });
 
-    const [identifier, token] = req.headers.authorization.split(" ");
+        const [identifier, token] = req.headers.authorization.split(" ");
 
-    if (identifier !== "Bearer")
-        return res.status(401).json({ error: "invalid token" });
+        if (identifier !== "Bearer")
+            return res.status(401).json({ error: "invalid token" });
 
-    const payload = verifyAccessToken(token);
+        const payload = verifyAccessToken(token);
 
-    if (!payload || !payload?.exp)
-        return res.status(401).json({ error: "invalid token" });
+        if (!payload || !payload?.exp)
+            return res.status(401).json({ error: "invalid token" });
 
-    if (Date.now() > payload.exp * 1000)
-        return res.status(401).json({ error: "token expired" });
+        if (Date.now() > payload.exp * 1000)
+            return res.status(401).json({ error: "token expired" });
 
-    next();
+        next();
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            console.error(err.message);
+            return res.status(500).json({ error: err.message });
+        }
+
+        return res.status(500).json({ error: "internal error" });
+    }
 };
 
 export { accessControl };
