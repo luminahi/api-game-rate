48c43490c747cbc11561262ab9f3acc2231f0adc
Alex Lima <delimaferreiraalex@gmail.com>
2024-06-01 19:21:20 -0300

    ref: include token generation service to user service

--- src/server/controllers/user/signIn.ts
+++ src/server/controllers/user/signIn.ts
@@ -1,6 +1,5 @@
 import { Handler } from "express";
 import { userService } from "../../database/services/user/index.js";
-import { generateAccessToken } from "../../database/services/user/generateAccessToken.js";
 
 const signIn: Handler = async (req, res, next) => {
     const { email, password } = req.body;
@@ -9,7 +8,7 @@ const signIn: Handler = async (req, res, next) => {
 
     if (result.isFailure()) return next(result);
 
-    const token = generateAccessToken(result.unwrap());
+    const token = userService.generateAccessToken(result.unwrap());
 
     return res.status(200).json({ token });
 };
--- src/server/database/services/user/index.ts
+++ src/server/database/services/user/index.ts
@@ -4,6 +4,7 @@ import { getByEmail } from "./getByEmail.js";
 import { getById } from "./getById.js";
 import { updateById } from "./updateById.js";
 import { verifyUser } from "./verifyUser.js";
+import { generateAccessToken } from "./generateAccessToken.js";
 
 const userService = {
     count,
@@ -12,6 +13,7 @@ const userService = {
     updateById,
     getByEmail,
     verifyUser,
+    generateAccessToken,
 };
 
 export { userService };
