d5c3f7ae2bef43ba474587f61130f8f3f30f29f7
Alex Lima <delimaferreiraalex@gmail.com>
2024-06-01 19:23:38 -0300

    tests: add signed user for further tests

--- tests/integration/games/create.test.ts
+++ tests/integration/games/create.test.ts
@@ -1,10 +1,37 @@
 import supertest from "supertest";
 import { server } from "../../../src/server/server.js";
 import { Game } from "../../../src/server/database/entities/game/Game.js";
+import { userService } from "../../../src/server/database/services/user/index.js";
+import { User } from "../../../src/server/database/entities/user/User.js";
 
 const testServer = supertest(server);
 
 describe("game POST", () => {
+    let accessToken = "";
+
+    beforeAll(async () => {
+        const user = new User();
+
+        user.email = "test@mail.com";
+        user.password = "10101010";
+        user.username = "fire test";
+
+        await userService.create(user);
+        accessToken = userService.generateAccessToken(user);
+    });
+
+    it("sends a valid body", async () => {
+        const game = new Game();
+        game.name = "game one";
+
+        const res = await testServer
+            .post("/api/v1/games")
+            .send(game)
+            .auth(accessToken, { type: "bearer" });
+
+        expect(res.status).toBe(201);
+    });
+
     it("sends a empty body without auth", async () => {
         const res = await testServer.post("/api/v1/games").send({});
 
