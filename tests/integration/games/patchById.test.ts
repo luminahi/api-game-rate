d7ab80e5b4e28fd5fd20b14e0e32bd775d54146e
Alex Lima <delimaferreiraalex@gmail.com>
2024-06-05 19:51:19 -0300

    tests: add game update/patch by id

---
+++ tests/integration/games/patchById.test.ts
@@ -0,0 +1,45 @@
+import { testServer } from "../testServer.js";
+import { getToken, insertUser, insertGameData } from "../testSetup.js";
+
+beforeAll(async () => insertUser());
+
+beforeEach(async () => insertGameData());
+
+describe("game update", () => {
+    const accessToken = getToken();
+
+    it("updates a single game", async () => {
+        const game = { name: "Cool Game" };
+
+        await testServer
+            .patch("/api/v1/games/1")
+            .send(game)
+            .auth(accessToken, { type: "bearer" })
+            .expect(204);
+
+        const res = await testServer
+            .get("/api/v1/games/1")
+            .auth(accessToken, { type: "bearer" })
+            .expect(200);
+
+        expect(res.body).toHaveProperty("game.name", "Cool Game");
+    });
+
+    it("tries to update a single game without auth", async () => {
+        const game = { name: "Cool Game" };
+
+        const res1 = await testServer
+            .patch("/api/v1/games/1")
+            .send(game)
+            .expect(401);
+
+        expect(res1.body).toHaveProperty("error", "not authorized");
+
+        const res2 = await testServer
+            .get("/api/v1/games/1")
+            .auth(accessToken, { type: "bearer" })
+            .expect(200);
+
+        expect(res2.body).not.toHaveProperty("game.name", "Cool Game");
+    });
+});
